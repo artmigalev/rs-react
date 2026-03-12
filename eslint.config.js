e4a07a039387d14277a1a9ca746b5370425936b8
artmigalev <art_migalev@yahoo.com>
2026-03-12 19:38:20 +0300

    fix: no unused & add base & add deploy

--- eslint.config.js
+++ eslint.config.js
@@ -48,12 +48,15 @@ export default defineConfig([
       'unused-imports': unusedImports,
     },
     rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+
       'unused-imports/no-unused-imports': 'error',
       'unused-imports/no-unused-vars': [
         'warn',
         {
           vars: 'all',
-          varsIgnorePattern: '^_',
+          // varsIgnorePattern: '^_',
+          varsIgnorePattern: '^React$',
           args: 'after-used',
           argsIgnorePattern: '^_',
         },
--- src/components/error-handling/error-btn/ErrorBtn.tsx
+++ src/components/error-handling/error-btn/ErrorBtn.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import styles from './ErrorBtn.module.css';
 export class ErrorBtn extends Component {
   state: Readonly<{ hasError: boolean }> = { hasError: false };
