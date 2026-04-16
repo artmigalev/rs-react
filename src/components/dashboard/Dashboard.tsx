f74d3e84fc4c29ff2a3e37af9c0a73911773ec8e
artmigalev <art_migalev@yahoo.com>
2026-04-16 15:45:54 +0300

    feat: download checked items by CSV format

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,9 +1,10 @@
 import { useAppDispatch, useAppSelector } from '@/utils/hooks/redux.hooks';
 import styles from './Dashboard.module.css';
 import { unselectAll } from '@/features/slicers';
+import { downloadCSV } from '@/utils/funcs/downloadCSV';
 
 const Dashboard = () => {
-  const count = useAppSelector((state) => state.checkedCards.length);
+  const listCards = useAppSelector((state) => state.checkedCards);
   const dispatch = useAppDispatch();
 
   return (
@@ -11,11 +12,11 @@ const Dashboard = () => {
       <h5 className={styles['title']}>Flyout</h5>
       <div className="flex flex-row w-full justify-between items-center gap-[var()]">
         <span className="text-2xl">
-          Selected Card: <span className="text-[var(--color-default)]">{count}</span>
+          Selected Card: <span className="text-[var(--color-default)]">{listCards.length}</span>
         </span>
         <div className="flex flex-row gap-[var(--gap-default)] ">
           <button onClick={() => dispatch(unselectAll())}>Unselect all</button>
-          <button>Download</button>
+          <button onClick={() => downloadCSV(listCards)}>Download</button>
         </div>
       </div>
     </section>
